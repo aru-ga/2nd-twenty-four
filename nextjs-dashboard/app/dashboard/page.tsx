a65e5d9ffe8a3949a681004a08eb8ddea44504e4
Muhammad Fargazzi <fargazzi@bull.co.id>
2024-01-15 07:40:35 +0700

    Update page.tsx

--- nextjs-dashboard/app/dashboard/page.tsx
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -1,3 +1,6 @@
 export default function Page(){
-    return <p>Dashboard Page</p>
+    return <>
+        <p>Dashboard Page</p>
+        <h5>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia dolorum temporibus debitis, pariatur nulla impedit accusantium necessitatibus veritatis! Voluptates, suscipit!</h5>
+    </>
 }
